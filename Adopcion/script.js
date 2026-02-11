562e2030c0d2a04adc45269cc4f797ddacf1271c
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2026-02-11 13:40:46 -0400

    Delete Adopcion/script.js

--- Adopcion/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('loginForm')?.addEventListener('submit', function(e) {
-    e.preventDefault();
-    console.log("Sincronizando con el Núcleo...");
-    // Aquí va tu fetch a /api/auth
-});
